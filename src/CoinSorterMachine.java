43e8372f5f6498274af683108f6400777ce8c8b3
Nikolas Larson <23larson@da.org>
2022-03-31 08:49:52 -0400

    March 31st, 2022. Final Push of Complete Lab

--- src/CoinSorterMachine.java
+++ src/CoinSorterMachine.java
@@ -1,7 +1,6 @@
 package src;
 
 import java.io.File;
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -17,51 +16,53 @@ public class CoinSorterMachine {
     private int halfDolNum;
     private int other;
 
-    // initializes coins ArrayList
+    /**
+     * initializes the coins ArrayList
+     */
     public CoinSorterMachine(){
         coins = new ArrayList<>();
     }
-    // use one or two Scanner objects, prompting user for the appropriate file
-// name and importing the data from filename – MUST handle diabolic values!
+
+    /**
+     * uses a Scanner object, prompting user for the appropriate file
+     * name and importing the data from filename – handles diabolic values
+     */
     public void depositCoins(){
         try {
             Scanner input = new Scanner(System.in);
-            pennyNum = 0;
-            nickelNum = 0;
-            dimeNum = 0;
-            quarterNum = 0;
-            dollarNum = 0;
-            halfDolNum = 0;
-            other = 0;
-            System.out.println("Enter the name of the data file for coin deposit: ");
+            pennyNum = 0; nickelNum = 0; dimeNum = 0; quarterNum = 0; dollarNum = 0; halfDolNum = 0;
+            System.out.print("Enter the name of the data file for coin deposit: ");
             String file = "datafiles/" + input.nextLine().toLowerCase();
             input.close();
             Scanner in = new Scanner(new File(file));
             System.out.println("Depositing coins...");
             while(in.hasNext()){
                 String number = in.next();
-                System.out.println(number);
-                if(number.equals("100")) {
-                    coins.add(new DollarCoin());
-                    dollarNum += 1;
-                }else if(number.equals("50")) {
-                    coins.add(new HalfDollar());
-                    halfDolNum += 1;
-                }else if(number.equals("25")) {
-                    coins.add(new Quarter());
-                    quarterNum += 1;
-                }else if(number.equals("10")) {
-                    coins.add(new Dime());
-                    dimeNum += 1;
-                }else if(number.equals("5")) {
-                    coins.add(new Nickel());
-                    nickelNum += 1;
-                }else if(number.equals("1")) {
-                    coins.add(new Penny());
-                    pennyNum += 1;
-                }else {
-                    System.out.println("Coin value " + number + " not recognized");
-                    other += Integer.parseInt(number)/100;
+                switch (number) {
+                    case "100" -> {
+                        coins.add(new DollarCoin());
+                        dollarNum += 1;
+                    }
+                    case "50" -> {
+                        coins.add(new HalfDollar());
+                        halfDolNum += 1;
+                    }
+                    case "25" -> {
+                        coins.add(new Quarter());
+                        quarterNum += 1;
+                    }
+                    case "10" -> {
+                        coins.add(new Dime());
+                        dimeNum += 1;
+                    }
+                    case "5" -> {
+                        coins.add(new Nickel());
+                        nickelNum += 1;
+                    }
+                    case "1" -> {
+                        coins.add(new Penny());
+                        pennyNum += 1;
+                    }
                 }
             }
         } catch(Exception e) {
@@ -73,30 +74,50 @@ public class CoinSorterMachine {
     }
 
 
-    // Prints deposit summary using a DecimalFormat object (see output section)
+    /**
+     * Prints deposit summary using a Numberformat object
+      */
     public void printDepositSummary(){
         System.out.println("Summary of deposit:");
         NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
-        double totalPen = 0.01*pennyNum;
-        System.out.println("\t" + pennyNum + " pennies " + (n.format(totalPen)));
-        double totalNick = 0.05*nickelNum;
-        System.out.println("\t" + nickelNum + " nickles " + (n.format(totalNick)));
-        double totalDim = 0.1*dimeNum;
-        System.out.println("\t" + dimeNum + " dimes " + (n.format(totalDim)));
-        double totalQuart = 0.25*quarterNum;
-        System.out.println("\t" + quarterNum + " quarters " + (n.format(totalQuart)));
-        double totalHalfDol = 0.5*halfDolNum;
-        System.out.println("\t" + halfDolNum + "  half dollar coins " + (n.format(totalHalfDol)));
-        double totalDol = dollarNum;
-        System.out.println("\t" + dollarNum + " dollar coins " + (n.format(totalDol)));
+        if(pennyNum ==1)
+            System.out.println("\t" + pennyNum + " pennie " + (n.format(0.01*pennyNum)));
+        else
+            System.out.println("\t" + pennyNum + " pennies " + (n.format(0.01*pennyNum)));
 
-        System.out.println("TOTAL DEPOSIT: " + n.format(totalPen+totalNick+totalDim+totalQuart+totalHalfDol+totalDol+other));
+        if(nickelNum == 1)
+            System.out.println("\t" + nickelNum + " nickle " + (n.format(0.05*nickelNum)));
+        else
+            System.out.println("\t" + nickelNum + " nickles " + (n.format(0.05*nickelNum)));
+
+
+        if(dimeNum == 1)
+            System.out.println("\t" + dimeNum + " dime " + (n.format(0.1*dimeNum)));
+        else
+            System.out.println("\t" + dimeNum + " dimes " + (n.format(0.1*dimeNum)));
+
+        if(quarterNum == 1)
+            System.out.println("\t" + quarterNum + " quarter " + (n.format(0.25*quarterNum)));
+        else
+            System.out.println("\t" + quarterNum + " quarters " + (n.format(0.25*quarterNum)));
+        if(halfDolNum == 1)
+            System.out.println("\t" + halfDolNum + " half dollar coin " + (n.format(halfDolNum*0.5)));
+        else
+            System.out.println("\t" + halfDolNum + " half dollar coins " + (n.format(halfDolNum*0.5)));
+        if(dollarNum == 1)
+            System.out.println("\t" + dollarNum + " dollar coin " + (n.format(dollarNum)));
+        else
+            System.out.println("\t" + dollarNum + " dollar coins " + (n.format(dollarNum)));
+        System.out.println("TOTAL DEPOSIT: " + n.format(getTotalValue()));
     }
 
 
     // return the total value of all Coin objects stored in coins as a double
     public double getTotalValue(){
-        return 0;
+        double val = 0;
+        for(Coin c : coins)
+            val += c.getValue();
+        return val;
     }
 
     // main method for the class should use these exact three lines of code
